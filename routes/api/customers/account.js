7bf77fb2112e66aab2c875cb8189703c34b50055
m.khani <root@localhost.localdomain>
2024-01-02 08:43:01 +0330

    edit post api

--- routes/api/customers/account.js
+++ routes/api/customers/account.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../../middleware/auth");
-
 const {loginPost} = require('../../../controller/api/V1/customers/account');
 const {registerPost} = require('../../../controller/api/V1/customers/account');
 const {dashboardPost} = require('../../../controller/api/V1/customers/account');
+const authIsNotLoggedIn = require("../../../middleware/authIsNotLoggedIn");
+const {editPost} = require("../../../controller/api/V1/customers/account");
 
 /*
  * @route /api/V1/customers/account/login
@@ -23,5 +24,9 @@ router.post("/register", registerPost);
  * @header x-access-token => your token
  */
 router.post("/dashboard",auth, dashboardPost);
+
+
+/* POST user edit form. */
+router.post("/edit", auth ,editPost);
 
 module.exports = router;
